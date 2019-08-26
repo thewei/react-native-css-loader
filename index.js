fc9a1c55cb38aaf8400a5be93471a44bf958c293
Nabil Tharwat <Nabil.tharwat@outlook.com>
2019-08-26 12:52:53 +0200

    Replaced variables with ES6 template literals

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-var parseCss = require('gulp-react-native-stylesheet-css/lib/parseCss');
+const parseCss = require('gulp-react-native-stylesheet-css/lib/parseCss')
 
-module.exports = function(source) {
-
-	var style = parseCss(source.replace(/\r?\n|\r/g, ""));
-
-	var prefix = "", suffix = "";
-	prefix = "var React = require('react-native');\nmodule.exports = React.StyleSheet.create(";
-	suffix = ");";
-	var content = prefix + style + suffix
-    return content;
-};
+module.exports = source => {
+	const style = parseCss(source.replace(/\r?\n|\r/g, ""))
+    	return `const React = require('react-native');\nmodule.exports = React.StyleSheet.create(${style});`
+}
